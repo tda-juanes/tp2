39e03a1f9d31306920082e056f010e3fb74b5f93
Manu Diaz <mdiaz@edrans.com>
2023-10-02 12:24:47 -0300

    reconstruir solucion - work in progress

--- main.py
+++ main.py
@@ -1,14 +1,27 @@
 import sys
 
-def mayor_ganancia(entrenamientos, energias):
+def calcular_ganancias(entrenamientos, energias):
     ganancias = [0]
 
-    for i in range(len(entrenamientos)):
+    for dia_actual in range(len(entrenamientos)):
         ganancias.append(max(ganancias))
-        for j in range(i):
-            ganancias[j] = ganancias[j] + min(energias[i - j], entrenamientos[i])
+        for i in range(dia_actual + 1):
+            ganancias[i] = ganancias[i] + min(energias[dia_actual - i], entrenamientos[dia_actual])
 
-    return max(ganancias)
+    return ganancias
+
+
+def reconstruir_solucion(ganancias, entrenamientos, energias):
+    dia_actual = len(entrenamientos) - 1
+    solucion = [True] * (dia_actual + 1)
+    while dia_actual > 0:
+        idx_max = ganancias.index(max(ganancias[:dia_actual]))
+        solucion[idx_max - 1] = False
+        for i in range(dia_actual, idx_max - 1, -1):
+            for j in range(idx_max - 1, -1, -1):
+                ganancias[i] -= min(entrenamientos[i], energias[i - j])
+        dia_actual = idx_max - 1
+    return reversed(solucion)
 
 
 def parse_input(file_h):
@@ -23,5 +36,13 @@ def parse_input(file_h):
         print('Invalid input: Line contents must be valid integers', file=sys.stderr)
 
 
+def main():
+    entrenamientos, energias = parse_input(sys.stdin)
+    ganancias = calcular_ganancias(entrenamientos, energias)
+    print('Ganancia maxima:', max(ganancias))
+    solucion = reconstruir_solucion(ganancias, entrenamientos, energias)
+    mapping = {True: 'Entreno', False: 'Descanso'}
+    print('Plan de entrenamiento:', ', '.join(map(mapping.get, solucion)))
+
 if __name__ == '__main__':
-    print(mayor_ganancia(*parse_input(sys.stdin)))
+    main()
